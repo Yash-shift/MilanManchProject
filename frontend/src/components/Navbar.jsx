f3d209ac42e752c18ea1a5dc51a7da9ec9ce7edc
Yash-shift <imyashdixit001@gmail.com>
2025-01-20 19:33:30 +0530

    navbar sticky

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -14,7 +14,7 @@ const Navbar = () => {
   };
 
   return (
-    <nav className="bg-white/80 backdrop-blur-md  w-full z-50 border-b border-amber-100">
+    <nav className="bg-white/80 backdrop-blur-md  w-full z-50 border-b border-amber-100 bg-white shadow-md sticky top-0">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex items-center justify-between h-16">
           <Link to="/" className="flex items-center">
